00d8eb011f35ee274a80f502c1f323b309929452
helun <938788163@qq.com>
2022-08-17 16:54:30 +0800

    feat: 新增husky

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never']
+  }
+}
+// feat: 新功能
+// fix: 修补bug
+// docs: 文档（documentation）
+// style: 样式/格式（不影响代码运行的变动）
+// refactor: 重构（即不是新增功能，也不是修改bug的代码变动）
+// test: 增加测试
+// chore: 构建过程或辅助工具的变动
